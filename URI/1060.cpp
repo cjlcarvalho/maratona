d26209e286b31ec62fa015614c6a85bf4c9433cb
Caio Jordão Carvalho <cjlcarvalho@live.com>
2017-08-21 19:00:30 -0300

    Update 1060.cpp

--- URI/1060.cpp
+++ URI/1060.cpp
@@ -3,13 +3,12 @@
 using namespace std;
 
 int main(){
-    double a = new double[6];
+    double a;
     int total = 0;
-    for(int i = 0; i<6; i++)
-        cin >> a[i];
-    for(double i : a)
-        if(i > 0)
-            total++;
+    for(int i = 0; i<6; i++){
+        cin >> a;
+        if(a > 0) total++;
+    }
     cout << total << " valores positivos" << endl;
     return 0;
 }
