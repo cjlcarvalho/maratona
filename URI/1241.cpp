6d22361383ae358eca444de250b03837b895c169
Caio Jordão Carvalho <cjlcarvalho@live.com>
2017-09-12 20:52:11 -0300

    URI training

--- URI/1241.cpp
+++ URI/1241.cpp
@@ -12,8 +12,8 @@ int main(){
 		cin >> a >> b;
 		bool equal = true;
 		for(int i = a.size() - 1, j = b.size() - 1; j >= 0; i--, j--) 
-            if(a[i] != b[j] || i < 0) 
-                equal = false;
+            		if(a[i] != b[j] || i < 0) 
+				equal = false;
 		if(equal)
 			cout << "encaixa" << endl;
 		else
