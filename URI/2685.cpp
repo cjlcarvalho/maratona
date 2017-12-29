5bef20b6226ffbce70359afe23e459bf9cef16e4
Caio <cjlcarvalho@live.com>
2017-12-29 00:33:16 -0300

    URI training

--- URI/2685.cpp
+++ URI/2685.cpp
@@ -1,13 +1,18 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     int m;
-    while(scanf(" %d", &m) != EOF){
-        m %= 360;
-        if(m < 90) printf("Bom Dia!!\n");
-        else if(m < 180) printf("Boa Tarde!!\n");
-        else if(m < 270) printf("Boa Noite!!\n");
-        else printf("De Madrugada!!\n");
+    while(cin >> m){
+        if(m >= 0){
+            if(m < 90) cout << "Bom Dia!!" << endl;
+            else if(m < 180) cout << "Boa Tarde!!" << endl;
+            else if(m < 270) cout << "Boa Noite!!" << endl;
+            else if(m <= 360) cout << "De Madrugada!!" << endl;
+        }
     }
     return 0;
 }
--- URI/2691.cpp
+++ URI/2691.cpp
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main(){
     int n, a, b;
@@ -6,13 +8,11 @@ int main(){
     while(n--){
         scanf(" %dx%d", &a, &b);
         if(a == b)
-            for(int i = a; i<=10; i++)
+            for(int i = 5; i<=10; i++)
                 printf("%d x %d = %d\n", a, i, a*i);
         else {
-            int m = max(a, b);
-            for(int i = m; i<=10; i++){
+            for(int i = 5; i<=10; i++)
                 printf("%d x %d = %d && %d x %d = %d\n", a, i, a*i, b, i, b*i);
-            }
         }
     }
     return 0;
