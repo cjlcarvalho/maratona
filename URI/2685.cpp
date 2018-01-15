e98eff6f344e5880f5ef1f9100ba91ab48dec54e
Caio Carvalho <caiojcarvalho@gmail.com>
2018-01-14 21:56:29 -0300

    URI training

--- URI/2685.cpp
+++ URI/2685.cpp
@@ -3,16 +3,12 @@
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
     int m;
     while(cin >> m){
-        if(m >= 0){
-            if(m < 90) cout << "Bom Dia!!" << endl;
-            else if(m < 180) cout << "Boa Tarde!!" << endl;
-            else if(m < 270) cout << "Boa Noite!!" << endl;
-            else if(m <= 360) cout << "De Madrugada!!" << endl;
-        }
+        if(m == 360 || (m >= 0 && m < 90)) cout << "Bom Dia!!" << endl;
+        else if(m >= 90 && m < 180) cout << "Boa Tarde!!" << endl;
+        else if(m >= 180 && m < 270) cout << "Boa Noite!!" << endl;
+        else if(m >= 270 && m < 360) cout << "De Madrugada!!" << endl;
     }
     return 0;
 }
