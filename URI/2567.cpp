4aa38fe040ab32f04ea43807c3fda4729734ea89
Caio Jordão Carvalho <caiojcarvalho@gmail.com>
2018-02-02 12:45:07 -0300

    URI training

--- URI/2567.cpp
+++ URI/2567.cpp
@@ -7,9 +7,9 @@ int main() {
     while (cin >> n) {
         vector<int> vet(n);
         for (int i = 0; i < n; i++) cin >> vet[i];
+        sort(vet.begin(), vet.end());
         int result = 0;
         while (vet.size() > 1) {
-            sort(vet.begin(), vet.end());
             result += abs(vet[vet.size() - 1] - vet[0]);
             vet.erase(vet.begin());
             vet.erase(vet.end() - 1);
