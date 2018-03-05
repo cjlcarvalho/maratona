911eb39ca452c881e12c533f0a93d0c22213020b
Caio Carvalho <caiojcarvalho@gmail.com>
2018-03-05 02:02:51 -0300

    URI training

--- URI/2570.cpp
+++ URI/2570.cpp
@@ -2,37 +2,34 @@
 
 using namespace std;
 
-int main(){
-    int n, m, k, l;
+int main() 
+{
+    int n, m, k, l, s = 1;
     char c;
-    cin >> n >> m >> k;
-    char rhcp[n][m];
-    char res[4] = { 'R', 'H', 'C', 'P' };
-    int s = 0;
-    
-    while(k--){
-        cin >> c >> l;
-        if(c == 'L')
-            memset(rhcp[l-1], res[s], m * sizeof(char));
-        else {
-            for(int i = 0; i<n; i++)
-                rhcp[i][l-1] = res[s];
+    int res[5] = { 0, 0, 0, 0, 0 };
+
+    scanf(" %d %d %d", &n, &m, &k);
+
+    int rhcp[n][m];
+    memset(rhcp, 0, sizeof(rhcp));
+    while(k--) {
+        scanf(" %c %d", &c, &l);
+        if(c == 'L') {
+            for (int i = 0; i < m; i++)
+                rhcp[l-1][i] = s;
         }
-        s++;
-        s %= 4;
+        else if (c == 'C') {
+            for(int i = 0; i < n; i++)
+                rhcp[i][l-1] = s;
+        }
+        s = (s + 1) % 5;
+        if (!s) s = 1;
     }
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            res[rhcp[i][j]]++;
     
-    int rt, ht, ct, pt;
-    rt = ht = ct = pt = 0; 
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<m; j++){
-            if(rhcp[i][j] == 'R') rt++;
-            else if(rhcp[i][j] == 'H') ht++;
-            else if(rhcp[i][j] == 'C') ct++;
-            else if(rhcp[i][j] == 'P') pt++;
-        }    
-    }
-    
-    cout << "R" << rt << " H" << ht << " C" << ct << " P" << pt << endl;
+    printf("R%d H%d C%d P%d\n", res[1], res[2], res[3], res[4]);
     return 0;
 }
