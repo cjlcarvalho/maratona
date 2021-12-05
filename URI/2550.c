693e290267220ef6fc4062b0feaafcb71a60a027
Caio Jordão Carvalho <caiojcarvalho@gmail.com>
2021-12-05 01:59:22 -0300

    removing unused variable

--- URI/2550.c
+++ URI/2550.c
@@ -9,18 +9,7 @@ typedef struct
     int w;
 } Edge;
 
-int adj[1000][1000];
-int visited[1000];
 int p[1000];
-int N;
-
-void dfs(int n) {
-    visited[n] = 1;
-    int i;
-    for (i = 0; i < N; i++)
-        if (adj[n][i] > 0 && !visited[i])
-            dfs(i);
-}
 
 int find(int n) {
     if (n == p[n])
@@ -47,46 +36,34 @@ int main()
     int n, m, x, y, w, sum, components, i;
 
     while (scanf("%d %d", &n, &m) != EOF) {
-        N = n;
-        memset(adj, 0, sizeof(adj));
-        memset(visited, 0, sizeof(visited));
         Edge edges[m];
-
-        components = 0;
 
         for (i = 0; i < m; i++) {
             scanf("%d %d %d", &x, &y, &w);
             edges[i].x = x - 1;
             edges[i].y = y - 1;
             edges[i].w = w;
-            adj[x - 1][y - 1] = w;
         }
 
-        for (i = 0; i < n; i++) {
-            if (!visited[i]) {
-                components++;
-                dfs(i);
+        for (i = 0; i < n; i++)
+            p[i] = i;
+
+        sum = 0;
+        components = n;
+        qsort(edges, m, sizeof(Edge), cmp);
+
+        for (i = 0; i < m; i++) {
+            if (!is_connected(edges[i].x, edges[i].y)) {
+                sum += edges[i].w;
+                union_(edges[i].x, edges[i].y);
+                components--;
             }
         }
 
         if (components > 1)
             printf("impossivel\n");
-        else {
-            for (i = 0; i < n; i++)
-                p[i] = i;
-
-            sum = 0;
-            qsort(edges, m, sizeof(Edge), cmp);
-
-            for (i = 0; i < m; i++) {
-                if (!is_connected(edges[i].x, edges[i].y)) {
-                    sum += edges[i].w;
-                    union_(edges[i].x, edges[i].y);
-                }
-            }
-
+        else
             printf("%d\n", sum);
-        }
 
     }
 
