96e2c1a12bfa8161e8c2778719740e8a84df57ab
Caio Jordão Carvalho <caiojcarvalho@gmail.com>
2021-12-09 10:20:22 -0300

    uri 2566 wip

---
+++ URI/2566.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int dist = INT_MAX;
+    Node* pred = nullptr;
+    map<int, int> adj;
+
+    void add_adj(int a, int r) {
+        adj[a] = r;
+    }
+
+    int W(int a) {
+        return adj[a];
+    }
+
+    bool operator>(const Node* b) const {
+        return dist > b->dist;
+    }
+};
+
+typedef vector<Node*> Graph;
+
+int dijkstra(Graph& graph) {
+    priority_queue<Node*, Graph, greater<Node*>> Q;
+
+    for (Node* node : graph)
+        Q.push(node);
+
+    bool arrived = false;
+
+    while(!Q.empty() && !arrived) {
+        Node* u = Q.top();
+        Q.pop();
+
+        for (auto& adj : u->adj) {
+            int weight = u->W(adj.first);
+            Node* v = graph[adj.first];
+            int dist = u->dist + weight;
+            if (dist < 0)
+                dist = INT_MAX;
+            if (v->dist > dist) {
+                v->dist = dist;
+                v->pred = u;
+                if (v == graph.back()) {
+                    arrived = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    return graph.back()->dist;
+}
+
+int main()
+{
+    int n, m, a, b, t, r;
+
+    while (cin >> n >> m) {
+        Graph nodes_bus;
+        Graph nodes_air;
+        for (int i = 0; i < n; i++) {
+            nodes_bus.push_back(new Node);
+            nodes_air.push_back(new Node);
+        }
+        nodes_bus[0]->dist = 0;
+        nodes_air[0]->dist = 0;
+
+        for (int i = 0; i < m; i++) {
+            cin >> a >> b >> t >> r;
+
+            if (t == 0)
+                nodes_bus[a - 1]->add_adj(b - 1, r);
+            else
+                nodes_air[a - 1]->add_adj(b - 1, r);
+        }
+
+        int cost_bus = dijkstra(nodes_bus);
+        int cost_air = dijkstra(nodes_air);
+
+        int min_cost = min(cost_bus, cost_air);
+
+        cout << min_cost << endl;
+
+        for (int i = 0; i < n; i++) {
+            delete nodes_bus[i];
+            delete nodes_air[i];
+        }
+    }
+
+    return 0;
+}
